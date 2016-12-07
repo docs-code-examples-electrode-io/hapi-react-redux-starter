7766b8c10e9f2049e6cb273d6b46165947d672ca
raydecastro <captray108@gmail.com>
2016-12-07 15:50:45 -0800

    feature. added SSR.

--- tools/hapi/server.js
+++ tools/hapi/server.js
@@ -5,7 +5,12 @@ import HapiWebpackDevMiddlewarePlugin from './hapiWebpackDevMiddlewarePlugin';
 import HapiWebpackHotMiddlewarePlugin from './hapiWebpackHotMiddlewarePlugin';
 import Config from '../../webpack.config.dev';
 
-/* eslint-disable no-console */
+import { renderToString } from 'react-dom/server';
+import { createStore } from 'redux';
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../../source/store/configureStore';
+import App from '../../source/components/App';
 
 const server = new Hapi.Server();
 const port = 3000;
@@ -38,13 +43,42 @@ server.register(require('inert'), (err) => {
   server.route({
     method: 'GET',
     path: '/{path*}',
-    handler: function (request, response) {
-      response.file(Path.join( __dirname, '../../source/index.html'));
-    }
+    handler: handleRender
   });
 });
 
+function handleRender(req, res) {
+  const store = configureStore();
+  const html = renderToString(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const preloadedState = store.getState();
+  res(renderFullPage(html, preloadedState));
+}
+
+function renderFullPage(html, preloadedState) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hapi React Redux Starter</title>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+        <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
+        </script>
+        <script src="/bundle.js"></script>
+      </body>
+    </html>
+    `;
+}
+
 server.start(function() {
-  console.log('server running @ ' + port);
+  /* eslint-disable no-console */
+
+  console.log('server running @ port ' + port);
   Open(`http://localhost:${port}`);
 });
